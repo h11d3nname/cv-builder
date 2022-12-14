2f357121b9b4ddbf8b4a871a1ca49d333800f3b3
Sophia Lis <h11d3nname@gmail.com>
2022-12-14 05:47:59 +0300

    cra init, base layout and deps

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { Header, Footer, Avatar } from './components';
+
+const App = () => {
+  const handleAvatarClick = () => console.log('avatar clicked');
+  const handlePrintClick = () => console.log('print button clicked');
+
+  return (
+    <div className='ui-wrapper'>
+      <Header onClick={handlePrintClick} />
+      <div className='ui-content-wrapper'>
+        <section className='ui-section'>
+          <div className='ui-container'>
+            <Avatar onClick={handleAvatarClick} />
+            <h1 className='ui-title-1'>Hello world!</h1>
+          </div>
+        </section>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import propTypes from 'prop-types';
+
+import './Header.scss';
+
+const Header = ({ onClick }) => {
+  return (
+    <header className='Header'>
+      <div className='ui-container'>
+        <div className='Header_content'>
+          <span className='Header_logo'>Cv builder</span>
+          <button className='ui-button isLink' onClick={onClick}>
+            Print
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+Header.propTypes = {
+  onClick: propTypes.func,
+};
+
+Header.defaultProps = {
+  onClick: () => {},
+};
+
+export default Header;
