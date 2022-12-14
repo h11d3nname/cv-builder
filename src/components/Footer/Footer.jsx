3c53fc30cf320a17099be6cedcd4d777045ecd8f
n1v0xin <n1v0xin.dev@gmail.com>
2022-12-14 05:47:59 +0300

    cra init, base layout and deps

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Footer.scss';
+
+const Footer = () => {
+  return (
+    <footer className='Footer'>
+      <div className='ui-container'>
+        <a className='ui-link' href='github.com' target='_blank'>
+          Github
+        </a>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
